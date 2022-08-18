0e07fa487e0ce217fde33b23ad7d3ba47685ebaa
Prajol10 <prajolghimire10023@gmail.com>
2022-08-18 11:32:39 +0545

    first commit

---
+++ 1.cpp
@@ -0,0 +1,38 @@
+//exammple of unary operator
+#include<iostream>
+using namespace std;
+class Counter{
+    private:
+    unsigned int count;
+    public:
+    Counter(){
+        count=0;
+
+    }
+    int get_count(){
+        return count;
+
+    }
+    void operator++(int){
+        count++;
+
+    }
+    void operator++(){
+        count++;
+    }
+    
+};
+int main(){
+    Counter c1,c2;
+    cout<<"/nc1="<<c1.get_count();
+    cout<<"/nc2="<<c2.get_count();
+    c1++;
+    c2++;
+    ++c2;
+     cout<<"/nc1="<<c1.get_count();
+    cout<<"/nc2="<<c2.get_count();
+    return 0;
+
+
+
+};
